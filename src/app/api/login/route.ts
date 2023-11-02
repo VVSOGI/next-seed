22baf67851c3531c96f0ce72581fabccfe812b75
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-11-02 17:10:37 +0900

    remove api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -9,20 +9,6 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
     const { email, password } = (await req.json()) as { email: string; password: string }
-    const user = await prisma.user.findUnique({ where: { email } })
 
-    if (!user) {
-        return new NextResponse('Error', { status: 404 })
-    }
-
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
-    if (!isPasswordMatch) {
-        throw NextResponse.json({ error: 'password not match' }, { status: 401 })
-    }
-
-    const token = jwt.sign({ email }, process.env.NODE_JWT_SECRET as string, {
-        expiresIn: '2d'
-    })
-
-    return NextResponse.json({ token })
+    return NextResponse.json('hello world')
 }

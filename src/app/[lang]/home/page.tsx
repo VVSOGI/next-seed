d5ef122166f16d2062b402565eab93b60a608960
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-12-01 14:55:33 +0900

    delete home

--- src/app/[lang]/home/page.tsx
+++ src/app/[lang]/home/page.tsx
@@ -9,22 +9,16 @@ interface Props {
 }
 
 export default async function page({ params: { lang } }: Props) {
-    const data = await getOrders()
+    // const data = await getOrders()
 
     return (
         <section>
             <div className={commonStyles.divider} />
             <div className={commonStyles.title}>Order</div>
             <div className={styles.orders}>
-                <div className={styles.ordersWrapper}>
-                    <Orders data={data.slice(2, 3)} />
-                </div>
-                <div className={styles.ordersWrapper}>
-                    <Orders data={data.slice(0, 2)} />
-                </div>
-                <div className={styles.ordersWrapper}>
-                    <Orders data={data.slice(1, 3)} />
-                </div>
+                <div className={styles.ordersWrapper}>{/* <Orders data={data.slice(2, 3)} /> */}</div>
+                <div className={styles.ordersWrapper}>{/* <Orders data={data.slice(0, 2)} /> */}</div>
+                <div className={styles.ordersWrapper}>{/* <Orders data={data.slice(1, 3)} /> */}</div>
             </div>
         </section>
     )
